f1ea93358419e00e4014841baa2c2081ecc33d33
AK <akabir0101@gmail.com>
2021-09-22 18:24:16 -0400

    downsampling done

---
+++ data_generators/downsample.py
@@ -0,0 +1,76 @@
+import sys
+sys.path.append("../alphafold_analysis_for_mutation")
+
+import pandas as pd
+
+# configs
+input_filepath = "data/ssym_classified_full.xlsx"
+stabilizing_downsampled_dfs_filepath = "data/ssym_downsampled_stabilizing.xlsx"
+destabilizing_downsampled_dfs_filepath = "data/ssym_downsampled_destabilizing.xlsx"
+
+def get_freq_by_seq_len(dfs):
+    """Returns the frequency of rows by unique sequence lengths
+
+    Args:
+        dfs (DataFrame): a DataFrame object
+
+    Returns:
+        key-value pair: sequence lengths, frequencey
+    """
+    unique_seq_lens = dfs["seq_len"].unique().tolist()
+    freq = []
+    for seq_len in unique_seq_lens:
+        freq.append(len(dfs[dfs["seq_len"]==seq_len]))
+    return {"unique_seq_lens": unique_seq_lens,
+            "freq": freq}
+    
+def downsample(dfs, n_samples):
+    """Downsample the data by n_samples without replacement.
+
+    Args:
+        dfs (DataFrace): a DataFrame object
+        n_samples (int): number of sample we want
+
+    Returns:
+        [type]: [description]
+    """
+    unique_mutations = set()
+    entries = []    
+    unique_seq_lens = dfs["seq_len"].unique().tolist()
+    unique_seq_lens.sort(reverse=True)  
+    while True:
+        for seq_len in unique_seq_lens:
+            if len(unique_mutations)==n_samples: 
+                return pd.concat(entries, ignore_index=True)
+            
+            temp_df = dfs[dfs["seq_len"]==seq_len].sample(n=1, replace=False)
+            mutation_key = temp_df["pdb_id"].values[0] + temp_df["inverse_pdb_id"].values[0]
+            
+            if mutation_key not in unique_mutations:
+                entries.append(temp_df)
+                unique_mutations.add(mutation_key)
+                print(unique_mutations)
+            
+            # return 0
+    
+    
+
+ssym_classified_dfs = pd.read_excel(input_filepath)
+# print(ssym_classified_dfs["is_destabilizing"])
+
+stabilizing_dfs = ssym_classified_dfs[ssym_classified_dfs["is_destabilizing"] == False]
+destabilizing_dfs = ssym_classified_dfs[ssym_classified_dfs["is_destabilizing"] == True]
+print(get_freq_by_seq_len(stabilizing_dfs))
+print(get_freq_by_seq_len(destabilizing_dfs))
+
+# downsample
+stabilizing_downsampled_dfs = downsample(stabilizing_dfs, n_samples=40)
+destabilizing_downsampled_dfs = downsample(destabilizing_dfs, n_samples=40)
+
+# save to excel
+stabilizing_downsampled_dfs.to_excel(stabilizing_downsampled_dfs_filepath, index=False)
+destabilizing_downsampled_dfs.to_excel(destabilizing_downsampled_dfs_filepath, index=False)
+
+# print heads
+print(stabilizing_downsampled_dfs.head())
+print(destabilizing_downsampled_dfs.head())
---
+++ data_generators/proteins_for_alphafold_ing.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("../alphafold_analysis_for_mutation")
+
+import pandas as pd
+import subprocess
+
+def run_command(command):
+   return subprocess.getoutput(command)
+
+# configs
+stabilizing_downsampled_dfs_filepath = "data/ssym_downsampled_stabilizing.xlsx"
+destabilizing_downsampled_dfs_filepath = "data/ssym_downsampled_destabilizing.xlsx"
+
+stabilizing_downsampled_dfs = pd.read_excel(stabilizing_downsampled_dfs_filepath)
+destabilizing_downsampled_dfs = pd.read_excel(destabilizing_downsampled_dfs_filepath)
+
+# listing the unique pdb_ids 
+stabilizing_downsampled_pdb_list =  stabilizing_downsampled_dfs["pdb_id"].unique().tolist()
+stabilizing_downsampled_inverse_pdb_list = stabilizing_downsampled_dfs["inverse_pdb_id"].unique().tolist()
+destabilizing_downsampled_pdb_list = destabilizing_downsampled_dfs["pdb_id"].unique().tolist()
+destabilizing_downsampled_inverse_pdb_list = destabilizing_downsampled_dfs["inverse_pdb_id"].unique().tolist()
+
+# print the num of unique pdbs and pdb_ids to fold using AlphaFold
+print(len(stabilizing_downsampled_pdb_list), stabilizing_downsampled_pdb_list)
+print(len(stabilizing_downsampled_inverse_pdb_list), stabilizing_downsampled_inverse_pdb_list)
+print(len(destabilizing_downsampled_pdb_list), destabilizing_downsampled_pdb_list)
+print(len(destabilizing_downsampled_inverse_pdb_list), destabilizing_downsampled_inverse_pdb_list)
+# 13 ['1amq', '2lzm', '1l63', '1ihb', '2rn2', '1iob', '1ey0', '1lz1', '4lyz', '1oh0', '1bni', '1vqb', '5pti']
+# 40 ['5eaa', '1l11', '1l85', '1mx4', '1gob', '1hib', '2eyo', '1lhk', '1ir8', '1dmm', '1bse', '1vqj', '1bpt', '1l46', '235l', '1mx6', '3aa5', '2f0i', '2hea', '1lsn', '1bns', '1vqg', '1nag', '1qit', '3c8s', '240l', '2eyp', '1gb5', '1iot', '1bsc', '1d2w', '2eyl', '1yan', '1flu', '1fan', '1qir', '1dye', '2ey5', '1gf9', '1flq']
+# 10 ['2lzm', '1l63', '1ihb', '2rn2', '1ey0', '1lz1', '4lyz', '1cey', '1oh0', '1rn1']
+# 40 ['1l33', '255l', '1mx2', '3aa3', '1syc', '1gfu', '1hem', '1e6l', '1dmq', '1rgc', '1l57', '1l59', '1rda', '2ey6', '1inu', '1l19', '115l', '1lav', '1ey5', '1e6m', '1l20', '1l68', '1kvc', '1kab', '149l', '1rbu', '1ios', '110l', '1rbs', '1sye', '1lhh', '1rdc', '1gby', '109l', '1e6k', '1l42', '3aa2', '1ey7', '1ouc', '1dyg']
+
+for pdb_id in destabilizing_downsampled_inverse_pdb_list:
+    print(pdb_id)
+    command = "cp ../protein_data/data/pdbs_clean/{}* data/pdbs/".format(pdb_id)
+    run_command(command=command)
